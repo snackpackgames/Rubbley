7e73a3e7f8afa31527a324e5ca84787b96578283
Brendon Roberto <orph3us01@gmail.com>
2020-12-28 15:47:19 -0500

    feat: add Attractor script and demo prefabs to Rubbley-Orbit-Debug

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    public const float G = 667.4f;
+    public Rigidbody body;
+    public Rigidbody gravigaTarget;
+    public float gravigaMultiplier = 1.0f;
+
+    void FixedUpdate()
+    {
+        Attractor[] attractors = FindObjectsOfType<Attractor>();
+        foreach (Attractor attractor in attractors)
+        {
+            if (this != attractor)
+            {
+                if (gravigaTarget == attractor)
+                {
+                    Attract(attractor, gravigaMultiplier);
+                }
+                else
+                {
+                    Attract(attractor);
+                }
+            }
+        }
+    }
+
+    void Attract(Attractor other, float currentGravigaMultipler = 1.0f)
+    {
+        Rigidbody otherBody = other.body;
+
+        Vector3 direction = body.position - otherBody.position;
+        float distance = direction.magnitude;
+
+        Debug.Log(string.Format("distance: {0}", distance));
+
+        float forceMagnitude = ((body.mass * otherBody.mass) / Mathf.Pow(distance, 2));
+
+        Debug.Log(string.Format("forceMagnitude: {0}", forceMagnitude));
+        Debug.Log(string.Format("gravigaMultiplier: {0}", gravigaMultiplier));
+
+        Vector3 force = direction.normalized * (forceMagnitude * currentGravigaMultipler);
+
+        Debug.Log(string.Format("force: ({0}, {1}, {2})", force.x, force.y, force.z));
+
+        otherBody.AddForce(force);
+    }
+}
