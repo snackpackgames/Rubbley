82d677668e56923e6199a5c1a9c0de852ab9a573
Brendon Roberto <orph3us01@gmail.com>
2021-08-29 00:27:56 -0400

    feat: add Dark Singularity assets and initial Gravity Well implementation

---
+++ Assets/Scripts/CreateGravityWell.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CreateGravityWell : MonoBehaviour
+{
+    public GameObject gravityWellPrefab;
+    public Camera referenceCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            HandleMouseButtonDown();
+        }
+    }
+
+    void HandleMouseButtonDown() {
+        Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, referenceCamera.nearClipPlane);
+
+        Vector3 worldPosition = referenceCamera.ScreenToWorldPoint(position);
+
+        Instantiate(gravityWellPrefab, worldPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/GravityWell.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityWell : MonoBehaviour
+{
+    public const float G = 66.74f;
+    public float massiveness = 1;
+    Transform location;
+
+    void FixedUpdate()
+    {
+        Attractor[] attractors = FindObjectsOfType<Attractor>();
+        foreach (Attractor attractor in attractors)
+        {
+            if (this != attractor)
+            {
+                Attract(attractor);
+            }
+        }
+    }
+
+    void Attract(Attractor other, float currentGravigaMultipler = 1.0f)
+    {
+        Rigidbody otherBody = other.body;
+
+        Vector3 direction = location.position - otherBody.position;
+        float distance = direction.magnitude;
+
+        Debug.Log(string.Format("distance: {0}", distance));
+
+        float forceMagnitude = ((massiveness * otherBody.mass) / Mathf.Pow(distance, 2)) * G;
+
+        Debug.Log(string.Format("forceMagnitude: {0}", forceMagnitude));
+
+        Vector3 force = direction.normalized * (forceMagnitude * currentGravigaMultipler);
+
+        Debug.Log(string.Format("force: ({0}, {1}, {2})", force.x, force.y, force.z));
+
+        otherBody.AddForce(force);
+    }
+}
